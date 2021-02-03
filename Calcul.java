6ae2cbe103f26b4efd674750c4bd98470a7ed902
elrik68 <elrikasselin@outlook.com>
2021-02-03 01:02:53 -0500

    ajout main

--- Calcul.java
+++ Calcul.java
@@ -2,4 +2,5 @@ public class Calcul {
     int a = 1;
     int b = a + 1;
     int c = a - 1;
+    int d = a + 2;
 }
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Début code
+	 */
+        int a = 1;
+        int b = a + 1;
+        int c = a - 1;
+    }
+}
